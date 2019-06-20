a4de067e8e2ebf8915ef021a351e54864928ba51
Thomas Kellough <Tomallama@Thomass-iMac.attlocal.net>
2019-06-19 21:37:43 -0500

    init commit

---
+++ Project 07 - Hacking With Swift/Project 07 - Hacking With Swift/Petitions.swift
@@ -0,0 +1,13 @@
+//
+//  Petitions.swift
+//  Project 07 - Hacking With Swift
+//
+//  Created by Thomas Kellough on 6/19/19.
+//  Copyright © 2019 Thomas Kellough. All rights reserved.
+//
+
+import Foundation
+
+struct Petitions: Codable {
+    var results: [Petition]
+}
